8c0399e1fbedb18299e3f4ecc1266877f2f9f06b
Haaashh <hannsherin@gmail.com>
2025-10-04 20:27:21 +0530

    homepage set up

--- memory_keeper/stories/models.py
+++ memory_keeper/stories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Memory(models.Model):
+    story_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.story_text[:50]
--- memory_keeper/stories/views.py
+++ memory_keeper/stories/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render 
+from .models import Memory # Import Memory model
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+def index(request):
+    memories = Memory.objects.all().order_by('-created_at') # Get all memories, newest first
+    context = {
+        'memories': memories
+    }
+    return render(request, 'home.html', context)
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login') # Redirect to login page after signup
+    template_name = 'signup.html'
